68cde121cd9ee38bdec98e46c0a79e5bc0db6df2
bosley <bosley117@gmail.com>
2020-12-19 14:10:33 -0500

    WOrking on it

--- src/basic_contour/contour.rs
+++ src/basic_contour/contour.rs
@@ -1,7 +1,17 @@
 
-struct Point {
-    x: i32,
-    y: i32
+use opencv::{
+    prelude::{
+        Mat,
+        MatTrait
+    },
+    imgproc,
+    core
+};
+
+
+pub struct Point {
+    pub x: i32,
+    pub y: i32
 }
 
 enum State {
@@ -9,61 +19,141 @@ enum State {
     Executing
 }
 
-pub struct  Contour <'a> {
+pub struct  Contour {
 
     points: Vec<Point>,
     state:  State,
     alpha:  f32,
-    beta:   u16,
-    gamma:  u16,
-    window_name: &'a str
+    beta:   i32,
+    gamma:  i32,
 }
 
-impl <'a> Contour <'a> {
+impl Contour {
 
-    //! Create a new contour - The window should be made prior to instantiation 
-    //! as it will modify the window to add sliders for the ABG parameters
-    pub fn new(window_name: &'a str) -> Self {
+    pub fn new() -> Self {
         Self {
             points: Vec::new(),
             state:  State::AcceptingInput,
             alpha:  0.2,
             beta:   1,
-            gamma:  100,
-            window_name: window_name
+            gamma:  100
         }
     }
 
-    pub fn start(&mut self) -> bool {
+    pub fn get_params(&self) -> (i32, i32, i32) {
+        ((self.alpha * 100.00) as i32, self.beta, self.gamma)
+    }
+
+    pub fn update_alpha(&mut self, alpha: i32) {
+        self.alpha = alpha as f32 / 100.00;
+    }
+
+    pub fn update_beta(&mut self, beta: i32) {
+        self.beta = beta;
+    }
+
+    pub fn update_gamma(&mut self, gamma: i32) {
+        self.gamma = gamma;
+    }
+
+    pub fn start(&mut self) {
         match self.state {
             State::AcceptingInput => {
                 self.state = State::Executing;
             },
             State::Executing => {
-                return false;
+                return;
             }
         }
-        true
     }
 
-    pub fn add_point(&mut self, x: i32, y: i32) -> bool {
+    pub fn reset(&mut self) {
+        self.alpha = 0.2;
+        self.beta  = 1;
+        self.gamma = 100;
+        self.points.clear();
+        self.state = State::AcceptingInput;
+    }
+
+    pub fn add_point(&mut self, x: i32, y: i32)  {
 
         match self.state {
             State::Executing => {
-                return false;
+                return;
             }
             _ => {}
         }
 
         self.points.push(Point{ x: x, y: y});
-
-        return true;
     }
 
-    pub fn step(&mut self) {
+    pub fn step(&mut self, sobel_image: &mut Mat) {
 
-        // Block until ready
-        // Step each thing minimizing the snakaroni
+        match self.state {
+            State::AcceptingInput => {
+                return;
+            }
+            _ => {}
+        }
+        
+        let height = sobel_image.rows();
+        let width  = sobel_image.cols();
 
+        let mut idx: u64 = 0;
+        for point in self.points.iter_mut() {
+
+            let mut start = Point{ x: 0, y: 0 };
+            let mut end   = Point{ x: 0, y: 0 };
+
+            if point.x - 3 > 0 {
+                start.x = point.x - 3;
+            }
+
+            if point.x + 4 > width {
+                end.x = width;
+            } else {
+                end.x = point.x + 4;
+            }
+
+            if point.y - 3 > 0 {
+                start.y = point.y - 3;
+            }
+
+            if point.y + 4 > height {
+                end.y = height;
+            } else {
+                end.y = point.y + 4;
+            }
+
+            let np = self.calculate_new_pos(idx, start, end);
+
+            point.x = np.x; point.y = np.y;
+
+            idx = idx + 1;
+        }
+
+    }
+
+    fn average_distance(&mut self) -> f32 {
+
+        let mut sum = 0.00;
+
+        for idx in 0..self.points.len()-1 {
+            sum = sum + ((
+            ( (self.points[idx].x - self.points[idx + 1].x) *  (self.points[idx].x - self.points[idx + 1].x) ) + 
+            ( (self.points[idx].y - self.points[idx + 1].y) *  (self.points[idx].y - self.points[idx + 1].y) )) as f32 ).sqrt();
+        }
+
+        sum = sum + ((
+        ( (self.points[self.points.len()-1].x - self.points[0].x) *  (self.points[self.points.len()-1].x - self.points[0].x) ) + 
+        ( (self.points[self.points.len()-1].y - self.points[0].y) *  (self.points[self.points.len()-1].y - self.points[0].y) )) as f32 ).sqrt();
+
+        return sum / self.points.len() as f32;
+    }
+
+    fn calculate_new_pos(&self, idx: u64, start: Point, end: Point ) -> Point {
+
+
+        Point{ x: 0, y: 0}
     }
 }
--- src/basic_contour/main.rs
+++ src/basic_contour/main.rs
@@ -1,11 +1,15 @@
 extern crate clap;
 use clap::{Arg, App};
 
+use std::sync::{ Arc, Mutex };
+
 extern crate opencv;
 extern crate imsource;
 
 use opencv::{
+    core,
     highgui,
+    imgproc,
     prelude::*,
 };
 
@@ -41,21 +45,108 @@ fn main() -> opencv::Result<()> {
     };
 
     let window = "basic contour";
+    let sobel_window = "sobel";
 
     highgui::named_window(window, 1)?;
+    highgui::named_window(sobel_window, 1)?;
 
-    let mut contour = Contour::new(window);
+    let contour = Arc::new(Mutex::new(Contour::new()));
 
-    loop {
+    let (mut alpha, mut beta, mut gamma) = contour.lock().unwrap().get_params();
+
+    let mut sobel_delta = 0;
+
+    let _ = highgui::create_trackbar("alpha", window, &mut alpha, 100, None);
+    let _ = highgui::create_trackbar("beta",  window, &mut beta,  100, None);
+    let _ = highgui::create_trackbar("gamma", window, &mut gamma, 100, None);
+
+    let _ = highgui::create_trackbar("s delta", window, &mut sobel_delta, 100, None);
+
+    highgui::set_mouse_callback(window, Some(Box::new({
+
+        let arc_contour = Arc::clone(&contour);
+
+        move |event, x, y, _flags| {
+            {
+                let mut arc_con = match arc_contour.lock() {
+                    Ok(g)  => g,
+                    Err(_) => return
+                };
+                
+                if event == highgui::EVENT_LBUTTONDOWN {
+
+                    println!("Clicked ({}, {})", x, y);
+                    arc_con.add_point(x, y);
+                }
+            }
+        }
+    })))?;
+
+    'execution : loop {
         let mut frame = f_source.get_image().unwrap();
+        let mut gray  = core::Mat::default()?;
+
+        imgproc::cvt_color(
+            &frame,
+            &mut gray,
+            imgproc::COLOR_BGR2GRAY,
+            0
+        )?;
+
+        let mut sobel_image = core::Mat::default().unwrap();
+        gray.copy_to(&mut sobel_image).unwrap();
+
+        imgproc::sobel(&gray, &mut sobel_image, 0, 1, 1, 3, 1.0, sobel_delta as f64, 0).unwrap();
+
+        highgui::imshow(sobel_window, &sobel_image)?;
+
+        match contour.lock() {
+            Ok(mut contour_inner)  => {
+                   contour_inner.update_alpha(alpha);
+                   contour_inner.update_beta(beta);
+                   contour_inner.update_gamma(gamma);
+                   contour_inner.step(&mut sobel_image);
+            },
+            Err(_) => {}
+        };
 
         if frame.size()?.width > 0 {
             highgui::imshow(window, &mut frame)?;
         }
         
         let key = highgui::wait_key(10)?;
-        if key > 0 && key != 255 {
-            break;
+
+        match key {
+
+            // Escape key
+            27 => { 
+                println!("Key: 'Escape'");
+                break 'execution;
+            }
+
+            // Space key
+            32 => { 
+                println!("Key: 'Space'");
+                match contour.lock() {
+                    Ok(mut contour_inner)  => {
+                        contour_inner.start();
+                    },
+                    Err(_) => {}
+                };
+            }
+
+            // 'r' key
+            114 => {
+                println!("Key: 'r'");
+                match contour.lock() {
+                    Ok(mut contour_inner)  => {
+                        contour_inner.reset();
+                    },
+                    Err(_) => {}
+                };
+            }
+
+            _ => {}
         }
     }
     Ok(())
